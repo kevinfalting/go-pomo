c9df4e59d025ea48b07470ba26c7a033982fb1af
Kevin Falting <kevinfalting@gmail.com>
2020-06-21 16:29:16 +0100

    add flags

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+)
+
+var (
+	rounds = flag.Int("rounds", 3, "The number of study rounds per cycle/long break.")
+	short  = flag.Int("short", 5, "The duration of the short breaks in minutes.")
+	long   = flag.Int("long", 15, "The duration of the long breaks in minutes.")
+	study  = flag.Int("study", 25, "The number of minutes per study session.")
 )
 
 func main() {
-	fmt.Println("Hello world")
+	flag.Parse()
 }
